3926cc91320a2394e190cbeafdc04c60267d6dee
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-22 11:57:22 -0300

    configure post task to list

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 
-export default props =>(
-    <div>
-        <h1>List  </h1>
-    </div>
+import IconButton from "../template/iconButton";
 
-)
+export default props =>{
+
+    const renderRows = () =>{
+        const list = props.list || []
+        return list.map(todo=>(
+            <tr key={todo._id}>
+                <td>{todo.description }</td>
+                <td>
+                    <IconButton style='sucess' icon='check'
+                    onClick={()=> props.handleRemove(todo)}></IconButton>
+                     <IconButton style='warning' icon='undo'
+                    onClick={()=> props.handleRemove(todo)}></IconButton>
+                     <IconButton style='danger' icon='trash-o'
+                    onClick={()=> props.handleRemove(todo)}></IconButton>
+                
+                </td>
+            </tr>
+        ))
+    }
+
+    return(
+    <table className="table">
+        <thead>
+            <tr>
+                <th>Descrição</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            {renderRows()}
+        </tbody>
+    </table>
+
+    )
+    }
